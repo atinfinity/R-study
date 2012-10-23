e4961ef0504df5a5deb971bd919be86d504f6209
dandelion <dandelion1124@gmail.com>
2012-10-23 23:34:14 +0900

    added "sort sample"

---
+++ 0001_sort/sort.R
@@ -0,0 +1,33 @@
+vec1<-c(1:10)  #generate vector(1...10)
+vec1 #display input vector
+result1<-sort(vec1,decreasing = TRUE) #sort
+result1 #display result vector
+###########################################################
+
+###########################################################
+# bubbleSort
+# @param input input vector
+# @return output output vector
+###########################################################
+bubbleSort<-function(input){
+  output<-input
+  length<-length(input)
+  for (i in 1:length){
+    j<-length
+    while(j>i){
+      if(output[j-1]<output[j])
+      {
+        temp<-output[j-1]
+        output[j-1]<-output[j]
+        output[j]<-temp
+      }
+      j<-j-1
+    }
+  }
+  return(output)
+}
+
+vec2<-c(1:10) #generate vector(1...10)
+vec2 #display input vector
+result2<-bubbleSort(vec2) #sort
+result2 #display output vector
